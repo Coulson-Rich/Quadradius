f494bca49191c38f55eb1ef7c7b64bca64f7cc47
Coulson Rich <coulsonrich5@gmail.com>
2023-11-18 19:31:05 -0700

    completed rock paper scissors game now on to Quadradius

--- ex_server_client/server.py
+++ ex_server_client/server.py
@@ -1,8 +1,9 @@
 import socket
 import _thread
-import sys
+from player import Player
+import pickle
 
-server = "IPV4 ADDRESS"
+server = "10.37.133.52"
 port = 5555
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -10,37 +11,31 @@ try:
     s.bind((server, port))
 except socket.error as e:
     str(e)
+
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-pos = [(0,0),(100,100)]
+players = [Player(0,0,50,50,(255,0,0)), Player(100,100,50,50,(0,0,255))]
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-def threaded_client(conn, currPlayer):
-    conn.send(str.encode(make_pos(pos[currPlayer])))
+def threaded_client(conn, player):
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[currPlayer] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
-                if currPlayer == 1:
-                    reply = pos[0]
+                if player == 1:
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
                 print("Received: ", data)
                 print("Sending : ", reply)
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
     print("Lost connection")
--- rock_paper_scissors/network.py
+++ rock_paper_scissors/network.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
 
+
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,16 +12,17 @@ class Network:
 
     def getP(self):
         return self.p
-    
+
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return pickle.loads(self.client.recv(2048))
+            return self.client.recv(2048).decode()
         except:
             pass
+
     def send(self, data):
         try:
-            self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(2048))
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(2048*2))
         except socket.error as e:
             print(e)
